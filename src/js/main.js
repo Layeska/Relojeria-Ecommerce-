8a22dd49463df3cd9d18902cf335cc9edc969fbb
Ana Juárez <lisa13suarez@gmail.com>
2022-12-30 11:45:24 -0600

    añadiendo código

---
+++ src/js/main.js
@@ -0,0 +1,195 @@
+//! Mostrar el menu
+const navMenu = document.querySelector("#nav-menu"),
+    navToggle = document.querySelector("#nav-toggle"),
+    navClose = document.querySelector("#nav-close");
+
+if(navToggle) {
+    navToggle.addEventListener("click", () => { 
+        navMenu.classList.add("show-menu"); }); 
+}
+
+if(navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu");
+    });
+}
+
+//! Ocultar el menu
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+    const navMenu = document.querySelector("#nav-menu");
+    navMenu.classList.remove("show-menu");
+}
+
+navLink.forEach(item => item.addEventListener("click", linkAction));
+
+//! Cambio de fondo del header
+const scrollHeader = () => {
+    const header = document.querySelector("#header");
+    this.scrollY >= 50 ? header.classList.add("scroll-header") : header.classList.remove("scroll-header");
+};
+
+window.addEventListener("scroll", scrollHeader);
+
+//! Swipper
+let swiper = new Swiper(".testimonial-swiper", {
+    spaceBetween: 30,
+    loop: "true",
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+//! new swipper
+let newSwipper = new Swiper(".new-swiper", {
+    spaceBetween: 24,
+    loop: "true",
+    breakpoints: {
+        576: { slidesPerView: 2 },
+        768: { slidesPerView: 3 },
+        1024: { slidesPerView: 4 },
+    }
+});
+
+//! Links activos
+const section = document.querySelectorAll("section[id]");
+const scrollActive = () => {
+    const scrollY = window.pageYOffset;
+
+    section.forEach(item => {
+        const activeHeigth = item.offsetHeight,
+            activeTop = item.offsetTop - 58,
+            activeId = item.getAttribute("id"),
+            activeClass = document.querySelector(`.nav__menu a[href*=${activeId}]`);
+
+            if(scrollY > activeTop && scrollY <= activeTop + activeHeigth) {
+                activeClass.classList.add("active-link");
+            } else {
+                activeClass.classList.remove("active-link");
+            }
+    });
+};
+
+window.addEventListener("scroll", scrollActive);
+
+
+//! Scroleo con la flecha hacia arriba
+const scrollUp = () => {
+    const roll = document.querySelector("#scroll-up");
+    this.scrollY >= 350 ? roll.classList.add("show-scroll") : roll.classList.remove("show-scroll");
+};
+
+window.addEventListener("scroll", scrollUp);
+
+//! show cart
+const cart = document.querySelector("#cart"),
+    cartShop = document.querySelector("#cart-shop"),
+    cartClose = document.querySelector("#cart-close");
+
+cartShop ? cartShop.addEventListener("click", () => cart.classList.add("show-cart")) : 0;
+cartClose ? cartClose.addEventListener("click", () => cart.classList.remove("show-cart")) : 0;
+
+//! cambio de tema
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun';
+
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+})
+
+//! Logica del carrito de compras
+const base_datos = [
+    {
+        id: 1,
+        nombre: "Spirit Rose",
+        precio: 1500,
+        imagen: "https://i.postimg.cc/KYhs38v1/product1.png"
+    },
+    {
+        id: 2,
+        nombre: "Khaki pilot",
+        precio: 1350,
+        imagen: "https://i.postimg.cc/qMXtHmhW/product2.png"
+    },
+    {
+        id: 3,
+        nombre: "Jubilee black",
+        precio: 1350,
+        imagen: "https://i.postimg.cc/vmDD9bqd/product3.png"
+    },
+    {
+        id: 4,
+        nombre: "Fosil me3",
+        precio: 650,
+        imagen: "https://i.postimg.cc/GpNtxVVT/product4.png"
+    },
+    {
+        id: 5,
+        nombre: "Jazzmaster",
+        precio: 1050,
+        imagen: "https://i.postimg.cc/YCRmYXtQ/featured1.png"
+    },
+    {
+        id: 6,
+        nombre: "Rose Gold",
+        precio: 850,
+        imagen: "https://i.postimg.cc/XvvB8wvm/featured3.png"
+    },
+    {
+        id: 7,
+        nombre: "Longines Rose",
+        precio: 980,
+        imagen: "https://i.postimg.cc/8CnJnjJ5/new1.png"
+    },
+    {
+        id: 8,
+        nombre: "Ingersoll",
+        precio: 250,
+        imagen: "https://i.postimg.cc/sgshBFCV/featured2.png"
+    },
+    {
+        id: 9,
+        nombre: "Portuguese rose",
+        precio: 1590,
+        imagen: "https://i.postimg.cc/yx0Ddjfg/new4.png"
+    }
+];
+
+function ver(){
+    window.open("../pages/details.html", '_blank');
+}
+
+/*let carrito = [];
+const divisa = "NIO";*/
+const divCarrito = document.querySelector(".home");
+
+loadEvents();
+
+function loadEvents() {
+    divCarrito.addEventListener("click", agregarProducto);
+}
+
+function agregarProducto(e) {
+    console.log(e.target);
+};
